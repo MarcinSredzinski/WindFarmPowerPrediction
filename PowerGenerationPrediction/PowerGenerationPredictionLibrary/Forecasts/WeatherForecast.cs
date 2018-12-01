7797cda67a246f351186b4b823d304f78489ed43
MarcinSredzinski <m.sredzinski94@gmail.com>
2018-12-01 23:53:22 +0100

    Using reflections to avoid making changes every time server response is changed.

---
+++ PowerGenerationPrediction/PowerGenerationPredictionLibrary/Forecasts/WeatherForecast.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace PowerGenerationPredictionLibrary.Forecasts
+{
+    class WeatherForecast<T> : ForecastsInterfaces.IWeatherForecast<T>
+    {
+        string _weatherJson;
+      
+        public string ForecastSourceName => typeof(T).Name;
+
+        public ILocalization Localization { get; set; }
+        
+        public T GetForecast(ForecastsInterfaces.IApiAddress apiAddress)
+        {
+            T weather;
+            _weatherJson = new WebClient().DownloadString(apiAddress.GenerateAdress());
+            weather = JsonConvert.DeserializeObject<T>(_weatherJson);
+            return weather;
+        }
+
+        public void Save(string archivePath)
+        {
+            File.WriteAllTextAsync(archivePath, _weatherJson);
+        }
+
+    }
+}
